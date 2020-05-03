f7f7bd9c390cd16086830e282f8a8178dd2445f1
Max Ivanov <59026966+wtf6yte@users.noreply.github.com>
2020-05-03 21:14:26 +0200

    v1.0.6.1

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const { Client } = require("discord.js");
 const { config } = require("dotenv");
-const { drops } = require('./drops');
-var prefix = "acry$ ";
+const { drops } = require('./json/drops');
+const fs = require('fs');
+const botSettings = JSON.parse(fs.readFileSync('./json/settings.json'))
+var prefix = botSettings.prefix;
 
 var bot = new Client({
     disableEveryone: true
@@ -12,8 +14,28 @@ config({
 })
 
 bot.on('ready', () => {
+    console.log("> bot started");
+    console.log("> status: acry$ help | online on " + bot.guilds.cache.size + " servers");
     bot.user.setStatus("online");
-    bot.user.setActivity("acry$ help | online on " + client.guilds.cache.size + " servers", { type: "WATCHING" });
+    bot.user.setActivity("acry$ help | online on " + bot.guilds.cache.size + " servers", { type: "WATCHING" });
+});
+
+bot.commands = Client.Collector();
+
+fs.readdir("./cmds/", (err, files) => {
+  if (err) console.error(err);
+
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  if(jsfiles <= 0) {
+    console.log("> Error: No commands to load!");
+    return;
+  }
+  console.log(`> loaded ${jsfiles.length} commands.`);
+
+  jsfiles.forEach((f, i) => {
+    let props = require(`./cmds/${f}`);
+    bot.commands.set(f, props);
+  });
 });
 
 bot.on("message", async msg => {
@@ -22,124 +44,8 @@ bot.on("message", async msg => {
         const args = msg.content.slice(prefix.length).split(' ');
         const cmd = args.shift().toLowerCase();
 
-        var date = new Date();
-        var timestamp = date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDate()).slice(-2) + 'T' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '.' + date.getMilliseconds() + 'Z';
 
-        switch(cmd) {
-            case "help":
-                var helpEmbed = {
-                  "color": 4889423,
-                  "timestamp": timestamp,
-                  "footer": {
-                    "icon_url": msg.author.avatarURL(),
-                    "text": msg.author.tag
-                  },
-                  "author": {
-                    "name": "acrys//bot commands" + title,
-                    "url": "https://acrysbot.xyz/commands",
-                    "icon_url": bot.user.avatarURL()
-                  },
-                  "fields": [{"name": "link to the commands list.", "value": "https://acrysbot.xyz/commands"}]
-                };
-                msg.channel.send({ embed: helpEmbed });
-                break;
-            case "drop":
-                var fieldsArr = [];
-                if(args.length > 0) {
-                    if(!isNaN(args[0])) {
-                        if(!(args[0] < 1)) {
-                            if(args[0] <= 10) {
-                                for(var i = 0; i <= args[0] - 1; i++) {
-                                    var dropI = Math.floor(Math.random() * drops.length + 1);
-                                    fieldsArr.push({"name": drops[dropI][0], "value": drops[dropI][1]});
-                                }
-                            } else {
-                                msg.channel.send({embed: errorEmbed('Specified arguments cannot be more than 10.', msg.author.avatarURL(), msg.author.tag)});
-                                break;
-                            }
-                        } else {
-                            msg.channel.send({embed: errorEmbed('Specified arguments cannot be less than 1.', msg.author.avatarURL(), msg.author.tag)});
-                            break;
-                        }
-                    } else {
-                        msg.channel.send({embed: errorEmbed('Specified arguments are not a number.', msg.author.avatarURL(), msg.author.tag)});
-                        break;
-                    }
-                } else {
-                    var dropI = Math.floor(Math.random() * drops.length + 1);
-                    fieldsArr = [{"name": drops[dropI][0], "value": drops[dropI][1]}];
-                }
 
-                msg.channel.send({ embed: {
-                    "color": 0x21a2d1,
-                    "timestamp": timestamp,
-                    "footer": {
-                      "icon_url": msg.author.avatarURL(),
-                      "text": msg.author.tag
-                    },
-                    "fields": fieldsArr
-                  }});
-                break;
-            case "color":
-                if(args.length > 0) {
-                  switch(args[0]) {
-                    case "alizarin": addRole("alizarin", msg, 0xe74c3c); break;
-                    case "pomegranate": addRole("pomegranate", msg, 0xc0392b); break;
-                    case "carrot": addRole("carrot", msg, 0xe67e22); break;
-                    case "pumpkin": addRole("pumpkin", msg, 0xd35400); break;
-                    case "sunflower": addRole("sunflower", msg, 0xf1c40f); break;
-                    case "orange": addRole("orange", msg, 0xf39c12); break;
-                    case "turquoise": addRole("turquoise", msg, 0x1abc9c); break;
-                    case "greensea": addRole("greensea", msg, 0x16a085); break;
-                    case "emerald": addRole("emerald", msg, 0x2ecc71); break;
-                    case "nephritis": addRole("nephritis", msg, 0x27ae60); break;
-                    case "peterriver": addRole("peterriver", msg, 0x3498db); break;
-                    case "belizehole": addRole("belizehole", msg, 0x2980b9); break;
-                    case "amethyst": addRole("amethyst", msg, 0x9b59b6); break;
-                    case "wisteria": addRole("wisteria", msg, 0x8e44ad); break;
-                    default:
-                      var colorHex = args[0].substring(1);
-                      if(isHexColor(colorHex)) {
-                        addRole(args[0], msg, parseInt(colorHex, 16));
-                      } else {
-                        msg.channel.send({embed: errorEmbed('Provided hex color code is invalid. Use \'acry$ help color\'.', msg.author.avatarURL(), msg.author.tag)});
-                      }
-                      break;
-                  }
-                } else {
-                    msg.channel.send({embed: errorEmbed('No color provided. Use \'acry$ help color\'.', msg.author.avatarURL(), msg.author.tag)});
-                }
-                break;
-            case "removecolor":
-              if(args.length > 0) {
-                switch(args[0]) {
-                  case "alizarin": removeRole("alizarin", msg); break;
-                  case "pomegranate": removeRole("pomegranate", msg); break;
-                  case "carrot": removeRole("carrot", msg); break;
-                  case "pumpkin": removeRole("pumpkin", msg); break;
-                  case "sunflower": removeRole("sunflower", msg); break;
-                  case "orange": removeRole("orange", msg); break;
-                  case "turquoise": removeRole("turquoise", msg); break;
-                  case "greensea": removeRole("greensea", msg); break;
-                  case "emerald": removeRole("emerald", msg); break;
-                  case "nephritis": removeRole("nephritis", msg); break;
-                  case "peterriver": removeRole("peterriver", msg); break;
-                  case "belizehole": removeRole("belizehole", msg); break;
-                  case "amethyst": removeRole("amethyst", msg); break;
-                  case "wisteria": removeRole("wisteria", msg); break;
-                  default:
-                    if(args[0].charAt(0) == "#" && isHexColor(args[0].substring(1))){
-                      removeRole(args[0], msg);
-                    } else {
-                      msg.channel.send({embed: errorEmbed('Specified role is not a color role.', msg.author.avatarURL(), msg.author.tag)});
-                    }
-                    break;
-                }
-              } else {
-                msg.channel.send({embed: errorEmbed('No role to remove provided.', msg.author.avatarURL(), msg.author.tag)});
-              }
-              break;
-        }
 });
 
 function errorEmbed(message, avatarURL, tag) {
@@ -229,4 +135,4 @@ function isHexColor (hex) {
       && !isNaN(Number('0x' + hex))
 }
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKENDEV);
---
+++ cmds/help.js
@@ -0,0 +1,7 @@
+module.exports.run = async (bot, message, args) => {
+
+};
+
+module.exports.help = {
+  name: "help"
+}
