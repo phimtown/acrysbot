20edd82999ce81a8dfc3499f8141cf9cbe2b2ad0
Max Ivanov <59026966+wtf6yte@users.noreply.github.com>
2020-05-19 22:45:51 +0200

    Revert "game removed"
    This reverts commit d7759b49a93a1142a493b05e7c12e9857379a5d3.

---
+++ cmds/game/claim.js
@@ -0,0 +1,52 @@
+const chars = require('../../json/chars');
+const fs = require('fs');
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, msg, args) => {
+    if (!args.length) {
+        msg.channel.send({
+            embed: embeds.errorEmbed('Missing character.', msg.author.avatarURL(), msg.author.tag)
+        }).then(async msg => msg.delete({timeout: 5000}));
+    } else {
+
+        fs.readFile('json/game/claimed.json', (err, data) => {
+            if (err) throw err
+            console.log(err)
+
+            if (!(data.toString().includes(args.toString()))) {
+                if (JSON.stringify(chars).includes(args.toString())) {
+                    fs.appendFile('json/game/claimed.json', JSON.stringify(args), (err) => {
+                        if (err) throw err
+                        console.log(err);
+
+                        const embed = new Discord.MessageEmbed()
+                        .setTimestamp()
+                        .setFooter(msg.author.tag, msg.author.avatarURL())
+                        .setDescription('Character was successfully claimed.')
+                        .addField('Claimed character: ', args);
+                        msg.channel.send(embed);
+                        fs.appendFile('json/game/' + msg.member.id + '.json', JSON.stringify(args), (err) => {
+                            if (err) throw err
+                            console.log(err);
+                        });
+                    });
+                } else {
+                    msg.channel.send({
+                        embed: embeds.errorEmbed('Character not found.', msg.author.avatarURL(), msg.author.tag)
+                    }).then(async msg => msg.delete({timeout: 5000}));
+                }
+            } else {
+                msg.channel.send({
+                    embed: embeds.errorEmbed('That character is already claimed.', msg.author.avatarURL(), msg.author.tag)
+                }).then(async msg => msg.delete({timeout: 5000}));
+            }
+        });
+    };
+}
+
+module.exports.help = {
+    name: "claim",
+    arguments: "<char>",
+    description: "claim a  character",
+    category: "game"
+}
