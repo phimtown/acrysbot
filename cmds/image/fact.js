4f5c74757f2a5bfc8da4b185783683ce27b4d721
Max Ivanov <59026966+wtf6yte@users.noreply.github.com>
2020-06-16 17:42:15 +0200

    fact cmd

---
+++ cmds/image/fact.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const embeds = require('../../utils/embeds.js');
+const needle = require('needle');
+
+module.exports.run = async (bot, msg, args) => {
+    if(args[0]) {
+        var string = "";
+        args.forEach(a => {
+            string += a + " ";
+        });
+        needle.get(`https://nekobot.xyz/api/imagegen?type=fact&text=${string}`, function(err, res) {
+            if (err && res.body.status != 200) {
+                console.log(err);
+                msg.channel.send({
+                    embed: embeds.errorEmbed('An error occured.', msg.author.avatarURL(), msg.author.tag)
+                }).then(async msg => msg.delete({timeout: 5000}));
+                return;
+            }
+            const embed = new Discord.MessageEmbed()
+            .setColor(0x53e677)
+            .setTimestamp()
+            .setFooter(msg.author.tag, msg.author.avatarURL())
+            .setTitle(`Fact`)
+            .setImage(res.body.message);
+            msg.channel.send(embed);
+        });
+        return;
+    }
+};
+
+
+module.exports.help = {
+  name: "fact",
+  arguments: "<text>",
+  description: "display your fax. \ntext: the fact, man. your fax.",
+  category: "image"
+}
